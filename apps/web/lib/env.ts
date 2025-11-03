da3c424c2054d6e6e66d9b1f7861d76f120d3537
diivvuu <divyanshubawa9@gmail.com>
2025-11-03 21:35:29 +0530

    api bases updated

--- apps/web/lib/env.ts
+++ apps/web/lib/env.ts
@@ -1,5 +1,5 @@
 export const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE ??
+  process.env.NEXT_PUBLIC_API_URL ??
   (process.env.NODE_ENV === 'production'
     ? 'https://api.knwdle.com'
     : 'http://localhost:4000');
